1557c1929902ea79425bcf74ab1ee98ee1e38dac
thea2805 <thea2805@gmail.com>
2022-06-28 16:49:19 +0200

    Added initial razor pages content

--- BED_handin3_Identity/Pages/Kitchen/Kitchen.cshtml.cs
+++ BED_handin3_Identity/Pages/Kitchen/Kitchen.cshtml.cs
@@ -5,8 +5,24 @@ namespace BED_handin3_Identity.Pages.Kitchen
 {
     public class KitchenModel : PageModel
     {
+        public string? Date { get; set; }
+        public string? DateReverse { get; set; }
+
         public void OnGet()
         {
+            Date = DateTime.Now.ToShortDateString();
+            DateReverse = DateTime.Now.ToString("yyyy-MM-dd");
+
+
+            //Call get method(date)
+            /*Display the following:
+                Expected number of guests
+                Adults checked in 
+                Children checked in 
+                Guests yet to check in   
+            */
+
+
         }
     }
 }
--- BED_handin3_Identity/Pages/Restaurant/Restaurant.cshtml.cs
+++ BED_handin3_Identity/Pages/Restaurant/Restaurant.cshtml.cs
@@ -1,12 +1,22 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BED_handin3_Identity.Pages.Restaurant
 {
+    [Authorize("CanEnterRestaurant")]
     public class RestaurantModel : PageModel
     {
+        public string Message { get; set; }
+
         public void OnGet()
         {
+            Message = "Hello";
+        }
+
+        public void OnPost()
+        {
+            Message = "Post used";
         }
     }
 }
